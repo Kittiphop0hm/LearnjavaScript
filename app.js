9e92e0efe947ad9b686869224060ad8400070dca
KittiphopOhm <kittiphop.sang@mail.kmutt.ac.th>
2024-06-14 13:28:36 +0700

    Object

---
+++ app.js
@@ -0,0 +1,8 @@
+//Object Prototype
+const student = {id: 66130500009, name: 'Kittiphop', lastname: 'Sriranonchai'};
+console.log(student);
+console.log(Object.keys(student));
+console.log(Object.values(student));
+console.log(typeof(student));
+console.log(Object.prototype.isPrototypeOf(student));
+console.log(Date.prototype.isPrototypeOf(student));
