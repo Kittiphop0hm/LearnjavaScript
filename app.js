809e1de2066e975bbe14e5d5a2f42d2317b1ab47
KittiphopOhm <kittiphop.sang@mail.kmutt.ac.th>
2024-06-14 13:34:50 +0700

    Object concept log values

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 //Object Prototype
-const student = {id: 66130500009, name: 'Kittiphop', lastname: 'Sriranonchai'};
+const student = {id: 66130500009, firstname: 'Kittiphop', lastname: 'Sriranonchai'};
 console.log(student);
 console.log(Object.keys(student));
 console.log(Object.values(student));
 console.log(typeof(student));
 console.log(Object.prototype.isPrototypeOf(student));
 console.log(Date.prototype.isPrototypeOf(student));
+
+//propoties values
+const someoneId = student.id;
+const someoneName = student['firstname']
+console.log(someoneId);
+console.log(someoneName);
+
