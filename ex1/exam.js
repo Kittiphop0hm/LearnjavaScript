e7dd8422a81e84ea4271db155f13c15b91ba3ae2
KittiphopOhm <kittiphop.sang@mail.kmutt.ac.th>
2024-07-05 16:27:26 +0700

    Create exam.js

---
+++ ex1/exam.js
@@ -0,0 +1,20 @@
+const calGrade = function(score) {
+    if (0 <= score && score < 50) return 'E'
+    if (50 <= score && score < 60) return 'D'
+    if (60 <= score && score < 70) return 'C'
+    if (70 <= score && score < 80) return 'B'
+    if (80 <= score && score <= 100) return 'A'
+    return 'X'
+}
+console.log(calGrade(60));
+
+
+function computeMagic(start, stop, stepOver) {
+    if (stop < 0) return -1
+    for (let i = 0; i < 10; i++ ) {
+        
+    }
+
+
+    
+}
