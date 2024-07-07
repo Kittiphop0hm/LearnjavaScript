8c73b444aa6ad0fd22ad61576fb35c7e0ef4658c
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2024-07-08 01:17:53 +0700

    ถึง arrow function

--- learnJS/week6.js
+++ learnJS/week6.js
@@ -27,3 +27,44 @@ let {content: {location: {country: student_coutry}}} = student
 console.log(header_coontent);
 console.log(artist);
 console.log(student_coutry);
+
+
+class Person {
+    constructor(firstname, lastname, dateOfBirth) {
+        this.firstname = firstname
+        this.lastname = lastname
+        this.dateOfBirth = dateOfBirth
+    }
+
+    getFullname() {
+        return `${this.firstname} ${this.lastname}` 
+    }
+
+    getAge() {
+        let milliOfToday = Date.now()
+        let milliOfBirthdate = this.dateOfBirth.getTime()
+        let diff = milliOfToday - milliOfBirthdate
+        let dateOfDiff = new Date(diff)
+        return dateOfDiff.getFullYear() - 1970
+    }
+
+    isEqual(anotherPerson) {
+        return this.firstname.toLowerCase() === anotherPerson.firstname.toLowerCase() 
+        && this.lastname.toLowerCase() === anotherPerson.lastname.toLowerCase()
+    }
+
+    toString() {
+        return `${this.getFullname()} ${this.getAge()}`
+    }
+}
+
+const person1 = new Person('Kittiphop', 'Sriranonchai', new Date(2005, 1, 25))
+const person2 = new Person('Typhoon', 'Herican', new Date(1987, 1, 1))
+console.log(person1.getAge());
+console.log(person2.getAge());
+console.log(person1.isEqual(new Person('Kittiphop', 'Sriranonchai', new Date(2005, 2, 25))));
+console.log(person1.toString());
+
+
+
+
