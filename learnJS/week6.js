b494478f425f49f36deb904cf4fc6699b1166f94
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2024-07-10 01:15:29 +0700

    Finish arrow function, forEach, filter and map

--- learnJS/week6.js
+++ learnJS/week6.js
@@ -65,6 +65,59 @@ console.log(person2.getAge());
 console.log(person1.isEqual(new Person('Kittiphop', 'Sriranonchai', new Date(2005, 2, 25))));
 console.log(person1.toString());
 
+//Arrow function
+//V1 Auto return
+const sum1 = (n1, n2) => n1 + n2
+//V2
+const sum2 = (n1, n2) => {
+    return n1 + n2
+}
+//V3 Have many logic *Fix {}*
+const sum3 = (n1, n2) => {
+    let add = 10
+    return add + n1 + n2
+}
 
+//forEach
+const fruits = ['banana', 'mango', 'orange']
+fruits.forEach((fruit) => console.log(fruit))
+let allFruit = ''
+fruits.forEach((fruit) => allFruit += fruit.toUpperCase() + ' ')
+console.log(allFruit);
 
+//filter
+const animes = ['Naruto', 'One piece', 'Doraemon', 'Yugi']
+const result = animes.filter((anime) => anime.length <= 4)
+console.log(result);
 
+//map
+const numbers = [2, 4, 6, 8]
+const map1 = numbers.map((number) => number * 2)
+console.log(map1);
+
+const students = [
+    {id: 1, name: 'Ohm'},
+    {id: 2, name: 'Smark'},
+    {id: 3, name: 'Vee'}
+]
+const map2 = students.map((student) => student.name).filter((student) => student.length <= 3)
+console.log(map2);
+
+//exam
+const persons = [
+    {id: 1, firstname: 'Somsak', lastname: 'Jaidee'},
+    {id: 2, firstname: 'Somchai', lastname: 'Dee'},
+    {id: 3, firstname: 'Somying', lastname: 'Jai'}
+]
+//1
+const dePerson = persons.filter((person) => person.firstname.toLowerCase().includes('de') || person.lastname.toLowerCase().includes('de'))
+console.log(dePerson);
+//2
+const fullname = persons.map((person) => `${person.lastname}, ${person.firstname}`)
+console.log(fullname);
+//3
+const newId = persons.map((person) => {
+    const currentYear = new Date(Date.now()).getFullYear()
+    return currentYear + '' + person.id
+})
+console.log(newId);
