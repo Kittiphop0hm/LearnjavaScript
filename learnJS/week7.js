8f2b7207c7f54e5cf860cc92ec6e2b37070f3a05
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2024-07-31 23:09:57 +0700

    Create week7.js

---
+++ learnJS/week7.js
@@ -0,0 +1,12 @@
+const arr1 = [1, 2, 3, 4, 5]
+const arr2 = [0, 2, 4, 6, 8]
+const map = arr1.map((arr) => arr + 1)
+console.log(map)
+
+const every = arr1.every((arr) => arr > 0 && arr <= 5)
+console.log(every)
+const some = arr2.some((arr) => arr === 0)
+console.log(some);
+
+const
+
