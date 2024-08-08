23971158f76b597d59d5e2b6b3ae9463972ed3b2
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2024-08-08 12:03:30 +0700

    Finish week1

--- JS/week1.js
+++ JS/week1.js
@@ -1,1 +1,21 @@
+const paragraph = document.getElementsByTagName("p")
+alert(paragraph[0].nodeName)
 console.log(`Hello World`)
+
+function calculator(op, num1, num2) {
+    return op(num1, num2)
+}
+
+const sum = (num1, num2) => num1 + num2
+let sums = sum()
+
+const subtract=(num1, num2) => {
+    return num1 - num2
+}
+
+const randomNumber = () => {
+    const ranNum = Math.floor(Math.random()*10)+1
+    return ranNum
+}
+
+console.log(randomNumber());
