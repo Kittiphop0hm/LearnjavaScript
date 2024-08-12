8fd4681ca6d6a501e118c8593d2ce901be0fb0b8
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2024-08-13 00:44:15 +0700

    Update week7.js

--- learnJS/week7.js
+++ learnJS/week7.js
@@ -14,3 +14,28 @@ console.log(isStartWithALetter)
 
 const isSomeStartWithALetter = names.some((name) => name.toLocaleLowerCase().startsWith('a'))
 console.log(isSomeStartWithALetter);
+
+//reduce
+const nums = [2,4,6,8]
+const reducer1 = nums.reduce((total, currentValue) => `${total}, ${currentValue}`)
+const reducer2 = nums.reduce((sum, currentNumber) => sum + currentNumber, 0)
+console.log(reducer1);
+console.log(reducer2);
+
+const names1 = ['Alice', 'Bob', 'Ann', 'Cath']
+const reducer3 = names1.reduce((concat, firstStr) => concat + firstStr[0], '')
+console.log(reducer3);
+
+const buyProducts = [
+    {price: 50, quantity: 2},
+    {price: 299, quantity: 10},
+    {price: 15, quantity: 5},
+]
+
+console.log(buyProducts);
+const reducer4 = buyProducts.reduce((result, currentValue) => result + (currentValue.price * currentValue.quantity), 0)
+console.log(reducer4);
+
+
+
+ 
