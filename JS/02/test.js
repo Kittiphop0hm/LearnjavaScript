ce580f2e04e0c5d482fad3edf62e2b90cc4226a5
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2024-08-18 00:14:30 +0700

    finish 1hr

---
+++ JS/02/test.js
@@ -0,0 +1,8 @@
+//synchronous program
+console.log(1-'1');
+//asynchronous program
+setTimeout(() => {
+    console.log('Introduction to JS');
+}, 5000)
+//synchronous program
+console.log('Hello World');
