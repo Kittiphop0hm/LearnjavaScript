92d16293ddbed49187e81fe40ef85af3074018ce
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2024-08-19 22:37:52 +0700

    Finish 1hr

--- JS/Patiphan/app.js
+++ JS/Patiphan/app.js
@@ -1,3 +1,14 @@
 const showMassage = () => {
     alert("External")
 }
+
+const myFunction = ()=> {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
+
+const myNameIs = () => {
+    let name = prompt("What your name")
+    document.getElementById("test").innerHTML = `My name is ${name}`
+}
+
+console.log(document.getElementById("demo"));
