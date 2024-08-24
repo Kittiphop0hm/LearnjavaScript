02460389efc36886f8618e683c3759d317981f7a
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2024-08-24 14:25:36 +0700

    Finish 1-2

---
+++ JS/03/exam.js
@@ -0,0 +1,31 @@
+//1
+function classicfyNumber(num) {
+    if (num > 0) return 'positive'
+    if (num < 0) return 'negative'
+    return 'zero'
+}
+
+console.log('---------- test 1 ----------');
+console.log(classicfyNumber(5));
+console.log(classicfyNumber(-3));
+console.log(classicfyNumber(0));
+
+//2
+function countVowels(str) {
+    const vowels = ['a','e','i','o','u']
+    let result = 0
+    vowels.forEach(vowel => {
+        if (str.toLowerCase().includes(vowel)) result++
+    });
+    return result
+}
+
+console.log(countVowels('hello'));
+console.log(countVowels('world'));
+console.log(countVowels(''));
+
+//3
+
+
+
+
