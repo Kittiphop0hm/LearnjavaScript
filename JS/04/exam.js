58ff4f9be3e380edd1d72edb5022cde39904b768
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2024-09-05 10:23:50 +0700

    finish practice

--- JS/04/exam.js
+++ JS/04/exam.js
@@ -1,6 +1,6 @@
 // Practice-01
 const combineArrays = (arr1, arr2) => {
-    return arr1.concat(arr2)
+    return [...arr1,...arr2]
 }
 console.log(combineArrays([1,2], [3,4]));
 console.log(combineArrays(['a','b'], ['c','d']));
@@ -16,3 +16,10 @@ console.log(getStringLength(['apple', 'banana', 'kiwi', 'orange']));
 console.log(getStringLength(['hello', 'world']));
 console.log(getStringLength(['ChatGPT', 'AI']));
 
+// Practice-03
+const greet = (name, greeting) => {
+    return `${greeting ?? 'Hello'}, ${name}!`
+}
+
+console.log(greet('Ohm', 'Hi'));
+console.log(greet('Ohm', null));
