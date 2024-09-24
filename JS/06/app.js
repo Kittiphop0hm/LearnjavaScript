650f74d5da9b62e1595214b6fabad73543519e0d
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2024-09-25 01:42:53 +0700

    Update app.js

--- JS/06/app.js
+++ JS/06/app.js
@@ -32,5 +32,25 @@ if (Object.keys(empObj).length === 0) {
     console.log('compare use Object.keys');
 }
 
+//Practice
+const extractAndRename = (obj) => {
+    return `{fullName:'${obj.name}',yearsOld:${obj.age}}`
+}
+console.log(extractAndRename({name:'John',age:25,city:'New York'}));
+console.log(extractAndRename({name:'Jane',age:30}));
 
+const mergeAndDestructure = (obj1, obj2) => {
+    let resultName = ''
+    let resultAge = 0
+    if (obj1.hasOwnProperty('name') && obj1.hasOwnProperty('age')) {
+        return `{name:${obj1.name},age:${obj1.age}}`
+    }
+    if (obj2.hasOwnProperty('name') && obj2.hasOwnProperty('age')) {
+        return `{name:${obj2.name},age:${obj2.age}}`
+    }
+    if (condition) {
+        
+    }
+}
 
+const obj = {name:'Jane',age:30}
