0dd72b314e5e10a1a8a39ee4f37fca7bfe866da5
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2024-10-12 00:54:23 +0700

    Finish practice

--- JS/09/app.js
+++ JS/09/app.js
@@ -1,3 +1,5 @@
+const { log } = require("console");
+
 //neted function
 function x(num) {
     function y() {
@@ -7,3 +9,52 @@ function x(num) {
 }
 
 console.log(x(5));
+
+//TodoList Practice
+class Todo {
+    constructor(id, description) {
+        this.id = id
+        this.description = description
+    }
+    getTodo = () => `${this.id} ${this.description}`
+    setDescription = (newDesription) => this.description = newDesription 
+}
+
+const todos = []
+let nextTodoId = 1
+const addTodo = (desc) => {
+    if (desc === null || desc === undefined) throw new Error("Desc can't valid")
+    if (todos.some((todo) => todo.id === nextTodoId)) {
+        throw Error("ID can't unique")
+    }
+    const newTodo = new Todo(nextTodoId, desc)
+    todos.push(newTodo)
+    nextTodoId++    
+    return todos.length
+}
+console.log('Test AddTodo:', addTodo("Hello World"))
+console.log('Test AddTodo:', addTodo("Hello JS"))
+todos.forEach(todo => console.log('Test AddTodo:', todo))
+
+const findTodo = (searchId) => {
+    return todos.find((todo) => todo.id === searchId)   
+}
+console.log('Test FindTodo:', findTodo(2));
+
+const findIndexTodo = (searchId) => {
+    return todos.findIndex((todo) => todo.id === searchId)
+}
+console.log('Test FindIndexTodo:', findIndexTodo(2));
+
+const removeTodo = (removeId) => {
+    const findIndex = todos.findIndex((todo) => todo.id === removeId)
+    return todos.splice(findIndex, 1)
+}
+removeTodo(2)
+console.log('Test Remove:', todos);
+
+const getTodos = () => {
+    return todos.forEach((todo) => console.log(`Id: ${todo.id} Description: ${todo.description}`))
+}
+console.log(addTodo("Hello Java"));
+getTodos()
