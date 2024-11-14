aa1cfeb85f2c6c5e52de087be14df97163b2ea77
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2024-11-14 11:59:42 +0700

    finish 12

---
+++ JS_DOM/12/app.js
@@ -0,0 +1,36 @@
+const course = document.getElementById("course");
+const subject = document.createElement("p");
+//set style of subject
+// subject.style.color = 'red'
+subject.setAttribute("style", "color:red")
+subject.textContent = "INT209-Network";
+course.appendChild(subject);
+
+let currentNode = course
+while(currentNode!==null) {
+    console.log(`current node: ${currentNode}, node type : ${currentNode.nodeType}, node name: ${currentNode.nodeName}, node value: ${currentNode.nodeValue}`);
+    console.log(`childCount: ${currentNode.childElementCount}`)
+    currentNode = currentNode.lastChild
+}
+
+const int202 = document.getElementsByName('serverSide')
+console.log(int202);
+const divElement = document.getElementsByTagName('div')
+console.log(divElement);
+console.log(course);
+const lastCourse = course[course.length-1];
+console.log(lastCourse);
+const int129 = document.getElementsByClassName('int129')
+console.log(int129);
+console.log(int129[int129.length]-1);
+
+//querySelector
+const allDiv = document.querySelectorAll('div')
+console.log(allDiv);
+
+
+
+
+
+
+
