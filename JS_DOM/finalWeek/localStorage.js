1cdc0530dfd75729287af7993b19e4202252e219
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2024-12-03 23:35:38 +0700

    finish cookie

--- JS_DOM/finalWeek/localStorage.js
+++ JS_DOM/finalWeek/localStorage.js
@@ -1,6 +1,15 @@
+import { CookieUtil } from './myLib/cookieUtil.js'
 document.cookie = "color=red";
 console.log(document.cookie);
-document.cookie = "theme=cartoon";
-document.cookie = `color=yallow;expires=${new Date(2021, 11, 4)}`;
+document.cookie = `theme=cartoon;max-age=${60*60}`;
+document.cookie = `color=yallow;expires=${new Date(2024, 11, 4)}`;
 console.log(document.cookie);
 document.cookie = `token-id=12345;HttpOnly`;
+document.cookie = `border=bold`
+console.log(document.cookie);
+
+//CookieUtil
+CookieUtil.set('color', 'black', new Date(2024, 11, 10))
+console.log(CookieUtil.get('color'))
+CookieUtil.unset('color')
+CookieUtil.unset('border')
